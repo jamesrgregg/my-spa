25bb8de2d44b72ac71c3c83b78d45bdcd5233fdb
James Gregg <james.r.greggaz68@gmail.com>
2025-01-30 15:41:03 -0700

    ci: Deploy App to GH Pages

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        {/* <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+      </div>
+      <h1>Arena Si DevOps LLC</h1>
+      <div className="card">
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button> */}
+        <p>
+          About Us
+        </p>
+        <p>
+          Services
+          <li>xxx</li>
+          <li>xxx</li>
+          <li>xxx</li>
+
+        </p>
+        <p>
+          Contact 
+        </p>
+        {/* <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p> */}
+      </div>
+      <p className="read-the-docs">
+        Enter a tag line here for the company
+      </p>
+    </>
+  )
+}
+
+export default App
